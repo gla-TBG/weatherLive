545e8eb2b8b86c22acea54ea8895f354a44b2ee5
zhongjunqi <zhongjunqi@iplas.com.cn>
2019-10-14 19:10:57 +0800

    test github page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 var cacheStorageKey='check-demo-2.1' //版本号，当想更新缓存资源（文件、数据等）
 var cacheList=[
@@ -19,44 +19,47 @@ self.addEventListener('install',function(e){
         )
 })
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.css'),
-    new workbox.strategies.NetworkFirst({
-        cacheName: cacheStorageKey
-    })
-);
-// //缓存通过fetch获取的数据
-// self.addEventListener('fetch',function(e){
-//     e.respondWith(
-//         caches.match(e.request).then(function(response){
-//             if(response!=null){
-//                 return response
-//             }
-//             // 因为 event.request 流已经在 caches.match 中使用过一次，
-//             // 那么该流是不能再次使用的。我们只能得到它的副本，拿去使用。
-//             var fetchRequest = e.request.clone();
-//             return fetch(fetchRequest)
-//                         .then(function(res){
-//                             // 检查是否成功
-//                             //失败了
-//                             if(!res || res.status !==200){
-//                                 return res
-//                             }
-//                             // 如果成功，该 response 一是要拿给浏览器渲染，二是要进行缓存。
-//                             // 不过需要记住，由于 caches.put 使用的是文件的响应流，一旦使用，
-//                             // 那么返回的 response 就无法访问造成失败，所以，这里需要复制一份。
-//                             var responseToCache = res.clone()
-//                             caches
-//                                 .open(cacheStorageKey)
-//                                 .then(function(cache){
-//                                     cache.put(e.request,responseToCache)
-//                                 });
-//                             return res
-//                 }
-//             )
-//         })
-//     )
-// })
+// workbox.routing.registerRoute(
+//     new RegExp('.*\.css'),
+//     new workbox.strategies.NetworkFirst({
+//         cacheName: cacheStorageKey
+//     })
+// );
+//缓存通过fetch获取的数据
+self.addEventListener('fetch',function(e){
+    if (e.request.url.indexOf('/main.css') !== -1) {
+        e.respondWith(
+            caches.match(e.request).then(function(response){
+                if(response!=null){
+                    return response
+                }
+                // 因为 event.request 流已经在 caches.match 中使用过一次，
+                // 那么该流是不能再次使用的。我们只能得到它的副本，拿去使用。
+                var fetchRequest = e.request.clone();
+                return fetch(fetchRequest)
+                    .then(function(res){
+                            // 检查是否成功
+                            //失败了
+                            if(!res || res.status !==200){
+                                return res
+                            }
+                            // 如果成功，该 response 一是要拿给浏览器渲染，二是要进行缓存。
+                            // 不过需要记住，由于 caches.put 使用的是文件的响应流，一旦使用，
+                            // 那么返回的 response 就无法访问造成失败，所以，这里需要复制一份。
+                            var responseToCache = res.clone()
+                            caches
+                                .open(cacheStorageKey)
+                                .then(function(cache){
+                                    cache.put(e.request,responseToCache)
+                                });
+                            return res
+                        }
+                    )
+            })
+        )
+    }
+
+})
 //当被激活时，检查版本资源，移除旧版本的资源
 self.addEventListener('activate',function(e){
     e.waitUntil(
